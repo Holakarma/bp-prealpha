37284b7898514cd7ab2e2cff83510bb487b40e9a
Holakarma <holakarmos@gmail.com>
2024-10-22 11:06:59 +0500

    init

---
+++ assets/ServicesPage-BcW5ASVY.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BQuGqfQh.js";import{S as e}from"./ServiceSection-C2Y3NFTT.js";import"./Typography-CJDrWVpL.js";const i=()=>r.jsx(e,{});export{i as default};
---
+++ assets/TutorialPage-DbQsLxMP.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BQuGqfQh.js";const a=()=>r.jsx("div",{children:"Tutorial page"});export{a as default};
